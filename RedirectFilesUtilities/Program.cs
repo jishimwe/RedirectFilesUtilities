24d0ed493b6b17444f1caf765c32fb54b042c792
jishimwe <jeanpaulishimwe@gmail.com>
2022-10-13 10:59:07 +0200

    first commit - libgit2charp implementation

---
+++ RedirectFilesUtilities/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+
+using LibGit2Sharp;
+using RedirectFilesUtilities;
+using static RedirectFilesUtilities.GhostRepository;
+
+namespace RedirectFilesUtilities
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GhostRepository ghostRepository = new GhostRepository(args[0], args[1]);
+        }
+    }
+}
