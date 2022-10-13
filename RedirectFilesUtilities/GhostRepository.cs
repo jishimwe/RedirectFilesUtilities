f57b0bf212f64e5c864f58ac68499e46a6d24c81
jishimwe <jeanpaulishimwe@gmail.com>
2022-10-13 16:17:37 +0200

    adding CloneEmptyRepository into GhostRepository

--- RedirectFilesUtilities/GhostRepository.cs
+++ RedirectFilesUtilities/GhostRepository.cs
@@ -10,11 +10,14 @@ namespace RedirectFilesUtilities
     internal class GhostRepository
     {
         private Repository repository;
+        private string remoteDefaultName = "origin";
 
 
         public GhostRepository(string repoURL, string destPath)
         {
-            repository = CloneGhostBranchRepository(repoURL, destPath);
+            //repository = CloneGhostBranchRepository(repoURL, destPath);
+            repository = CloneEmptyRepository(repoURL, destPath);
+            Console.WriteLine("Repository cloned");
         }
 
         private Repository CloneGhostBranchRepository(string repoURL, string destPath)
@@ -29,10 +32,25 @@ namespace RedirectFilesUtilities
             return new Repository(destPath);
         }
 
+        private Repository CloneEmptyRepository(string repoURL, string destPath)
+        {
+            string rootedPath = Repository.Init(destPath);
+            string logMessage = "fetch test";
+
+            Repository repo = new Repository(destPath);
+            //Remote remote = repo.Network.Remotes[remoteDefaultName];
+            Remote remote = repo.Network.Remotes.Add(remoteDefaultName, repoURL);
+            var refSpec = remote.FetchRefSpecs.Select(x => x.Specification);
+            Commands.Fetch(repo, remote.Name, refSpec, null, logMessage);
+            return repo;
+            //return null;
+        }
+
         private Branch CheckoutBranch(string bracnhName)
         {
             var branch = repository.Branches[bracnhName];
             if (branch == null) return null;
+            CheckoutOptions co = new CheckoutOptions();
             return Commands.Checkout(repository, branch);
         }
     }
